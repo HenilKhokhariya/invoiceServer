996190a0bdf3a64cacfe68f5327c1c5c91851cd6
Henil Khokhariya <henilkhokhariya27@gmail.com>
2024-08-01 17:14:07 +0530

    Update router.js

--- router.js
+++ router.js
@@ -37,9 +37,9 @@ const ensureDirectoryExistence = (dir) => {
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    // const dir = path.resolve(__dirname, "Image/Logo");
-    // ensureDirectoryExistence(dir);
-    cb(null, "./Image/Logo");
+    const dir = path.resolve(__dirname, "Image/Logo");
+    ensureDirectoryExistence(dir);
+    cb(null, dir);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now();
