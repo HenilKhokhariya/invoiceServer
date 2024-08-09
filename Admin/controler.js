e8fc89cdcce388953beba7b11743e759cd176f6c
HenilKhokhariya <henilkhokhariya27@gmail.com>
2024-08-09 17:02:52 +0530

    done

--- Admin/controler.js
+++ Admin/controler.js
@@ -29,7 +29,7 @@ const AdminCreateOtp = async(req,res)=>{
             otp
         }
         sendRegister.main(email,otp,a_name);
-        const token = jwt.sign(playload,jwt_key)
+        const token = jwt.sign(playload,jwt_key,{expiresIn:'5m'})
         return res.status(200).json({status:true,message:"Otp Send Successfully !",token});
     } catch (error) {
         console.log(error);
@@ -37,4 +37,38 @@ const AdminCreateOtp = async(req,res)=>{
     }
 }
 
-module.exports = {Home,AdminCreateOtp};
+const AdminCreate = async(req,res)=>{
+    try {
+        const token = await req.headers.authorization.split(' ')[1];
+        const otp = await JSON.parse(req.body.otp);
+        jwt.verify(token,jwt_key);
+        const data = jwt.decode(token,jwt_key);
+        const compare = data.otp == otp;
+        if(!compare)
+        {
+        return res.status(400).json({status:false,message:"Otp Invalid"});
+        }
+        const email = data.email;
+        const a_name = data.a_name;
+        const pw = data.password;
+        const password = cryptr.encrypt(pw);
+        const userExist = await Admin.findOne({email});
+        if(userExist){
+            return res.status(400).json({status:false,message:"Email Already Exist !"});
+        }
+        await Admin.create({a_name,email,password})
+        const playload= {
+            email,
+            a_name,
+            password
+        }
+        sendRegister.main(email,otp,a_name);
+        const newtoken = jwt.sign(playload,jwt_key);
+        return res.status(200).json({status:true,message:"Register Successfully !",token:newtoken});
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({status:false,message:"Otp Expier"});
+    }
+}
+
+module.exports = {Home,AdminCreateOtp,AdminCreate};
